0994544c3a5e8460967dfca91ab5c0c7a0600225
MansourMoukdad88 <m_sy@outlook.com>
2019-02-14 01:52:49 +0200

    Connect sercer to DB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-const express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
+var repos = require('../database-mysql');
+
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
@@ -13,6 +16,13 @@ app.post('/repos', function (req, res) {
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+    repos.selectAll(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 let port = 1128;
