d6b0e677f3ff01300aeb3902b79464b7851c85d6
MansourMoukdad88 <m_sy@outlook.com>
2019-02-18 22:25:36 +0200

    connect DB to phpMyAdmin + create two tables

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "mansour"
+  host: "sql7.freemysqlhosting.net",
+  user: "sql7279609",
+  password: "6gi2mD4wup",
+  database: "sql7279609"
 });
 
 connection.connect(err => {
@@ -15,16 +15,54 @@ connection.connect(err => {
 
 //console.log("FROM database .... : ", connection)
 
-var selectAll = function(callback) {
-  connection.query("SELECT * FROM repos", function(err, results, fields) {
+var getRepos = function(name, callback) {
+  console.log("Name ..... ", name);
+  connection.query(
+    `SELECT * FROM users JOIN repos ON users.user_id = repos.user_id WHERE users.name = "${name}"`,
+    function(err, results, fields) {
+      if (err) {
+        console.error(err);
+        callback(err, null);
+      } else {
+        console.log("Query From database", results);
+        callback(null, results);
+      }
+    }
+  );
+};
+
+var save = data => {
+  console.log("AAAAAAAAA", data.username);
+  var insert_User_Query = `INSERT INTO users (name) VALUES('${data.username}')`;
+  // var insert_Repos_Query = `INSERT INTO repos (username) VALUES('${data.name}')`;
+
+  connection.query(insert_User_Query, (err, results) => {
     if (err) {
-      console.error(err);
-      callback(err, null);
+      console.log(err);
+      //return res.send(err);
     } else {
-      console.log("Query From database", results);
-      callback(null, results);
+      //console.log("successfully added usernames", results);
+      //console.log("successfully added usernames", results.insertId);
+      console.log("mmmmmmmm", results);
+      //console.log("REPOOOOOOS", data.userRepo[0]["name"]);
+
+      for (var i = 0; i < data.userRepo.length; i++) {
+        var insert_Repos_Query = `INSERT INTO repos (repo_name, user_id) VALUES('${
+          data.userRepo[i]["name"]
+        }','${results.insertId}')`;
+        connection.query(insert_Repos_Query, function(err, results) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Saved to DB....");
+          }
+        });
+      }
+
+      // return res.send(data);
     }
   });
 };
 
-module.exports.selectAll = selectAll;
+module.exports.getRepos = getRepos;
+module.exports.save = save;
